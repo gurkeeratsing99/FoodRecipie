fafb9e204fce06da83303e75720eae9f1831684b
gurkeeratsing99 <gurkeeratsingh99@gmail.com>
2023-03-14 21:49:58 -0700

    Working with drawer Nav

--- app/src/main/java/com/example/projectfoodrecipie/HomeActivity.java
+++ app/src/main/java/com/example/projectfoodrecipie/HomeActivity.java
@@ -1,14 +1,84 @@
 package com.example.projectfoodrecipie;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
+import android.widget.Toolbar;
+
+import com.google.android.material.navigation.NavigationView;
 
 public class HomeActivity extends AppCompatActivity {
 
+    DrawerLayout drawerLayout;
+    NavigationView navigationView;
+    Toolbar toolbar;
+    ActionBarDrawerToggle actionBarDrawerToggle;
+    ImageView imgView;
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item){
+        if (actionBarDrawerToggle.onOptionsItemSelected(item)){
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        drawerLayout = findViewById(R.id.my_drawer_layout);
+        navigationView = findViewById(R.id.navigationView);
+        imgView = findViewById(R.id.imageMenu);
+        actionBarDrawerToggle = new ActionBarDrawerToggle(this,drawerLayout,R.string.menu_Open,R.string.close_menu);
+        drawerLayout.addDrawerListener(actionBarDrawerToggle);
+        actionBarDrawerToggle.syncState();
+
+        imgView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                drawerLayout.openDrawer(GravityCompat.START);
+            }
+        });
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+                switch (item.getItemId()){
+                    case R.id.nav_account:
+                        Toast.makeText(HomeActivity.this, "Selected Account", Toast.LENGTH_SHORT).show();
+                        break;
+
+                    case R.id.nav_settings:
+                        Toast.makeText(HomeActivity.this, "Selected Settings", Toast.LENGTH_SHORT).show();
+                        break;
+
+                    case R.id.nav_logout:
+                        Toast.makeText(HomeActivity.this, "Selected Logout", Toast.LENGTH_SHORT).show();
+                        break;
+
+
+                }
+
+                return true;
+            }
+
+        });
+
+
     }
+
+
+
 }
